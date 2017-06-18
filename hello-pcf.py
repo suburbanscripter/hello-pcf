de1276799f3f41ea6d1d97fbdd51c2ed5ee5d0af
suburban-coder <jeffreydahan@gmail.com>
2017-06-17 23:25:00 -0400

    added port information

--- hello-pcf.py
+++ hello-pcf.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 port = int(os.getenv("VCAP_APP_PORT"))
 
 @app.route('/')
-def hello-pcf():
-    return 'Hello Pivotal Cloud Foundry'
+def hello_pcf():
+    return 'Hello Pivotal Cloud Foundry\nPort: ' + str(port)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
